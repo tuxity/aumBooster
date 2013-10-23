7e38cad410449b0cf87b54bcba28df050c8ff261
Tuxity <kevin.darcel@gmail.com>
2013-10-23 23:56:14 +0200

    Missing typo from previous commit

--- aumBooster.php
+++ aumBooster.php
@@ -333,7 +333,7 @@ class aumBooster
                     }
                 }
 
-                preg_match('/var members = (.*);/', $this->client->getResponse()->getContent(), $matches);
+                preg_match('/var members = (.*),/', $this->client->getResponse()->getContent(), $matches);
                 $res = json_decode($matches[1]);
                 if (empty($res->members)) // in case we reach the max of pages, so break and do again a research
                     break;
